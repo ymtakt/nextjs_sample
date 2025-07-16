b12b6eaea29da5db668f3a20d48c2df5a2473b9e
西川雅人 <masato.nishikawa@arteryex.biz>
2025-07-16 17:09:09 +0900

    feat:ルーティングとCSSにカラーの追加

---
+++ src/constants/routes.ts
@@ -0,0 +1,24 @@
+/**
+ * ルーティング
+ */
+export const ROUTES = {
+  HOME: "/",
+  // ユーザー管理
+  USER_MANAGEMENT: "user-management",
+  // 検査管理
+  INSPECTION_MANAGEMENT: "inspection-management",
+  // 相談予約管理
+  CONSULTATION_BOOKING: "consultation-booking",
+  // 動画管理
+  VIDEO_MANAGEMENT: "video-management",
+  // お知らせ管理
+  NOTIFICATION_MANAGEMENT: "notification-management",
+  // 紹介コード管理
+  REFERRAL_CODE_MANAGEMENT: "referral-code-management",
+  // サプリメント管理
+  SUPPLEMENT_MANAGEMENT: "supplement-management",
+  // アカウント管理
+  ACCOUNT_MANAGEMENT: "account-management",
+  // ログアウト
+  // LOGOUT: 'logout',
+};

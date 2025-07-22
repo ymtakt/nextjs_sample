6d774829baa29a95afaf6656d9536962ca17cf4f
西川雅人 <masato.nishikawa@arteryex.biz>
2025-07-22 11:00:24 +0900

    feat:コメントボックスに文字数表示を追加

--- src/app/(test)/comp-test/comment-box/page.tsx
+++ src/app/(test)/comp-test/comment-box/page.tsx
@@ -6,15 +6,23 @@ import CommentBox from "@/components/general/FormParts/CommentBox";
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
   return (
-    <div className="ml-5 my-6 text-black">
+    <div className="ml-5 my-6  space-y-2 text-black">
       <h2 className="text-xl font-bold mb-4">📌 コメントボックス テスト</h2>
 
       <div className="flex flex-col space-y-4">
         <CommentBox
+          label="コメント"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
       </div>
+      <CommentBox
+        label="コメント 文字数表示付き"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        isMaxLength={true}
+        maxLength={300}
+      />
     </div>
   );
 }
--- src/components/general/FormParts/CommentBox/index.tsx
+++ src/components/general/FormParts/CommentBox/index.tsx
@@ -2,18 +2,24 @@
 
 type CommentBoxSizeProps = {
   value: string;
+  label?: string;
   placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   isDisabled?: boolean;
   isRounded?: boolean;
+  isMaxLength?: boolean;
+  maxLength?: number;
 };
 
 export default function CommentBox({
   placeholder = "コメントを入力してください",
   value,
+  label,
   onChange,
   isDisabled = false,
   isRounded = true,
+  isMaxLength = false,
+  maxLength = 10000,
 }: CommentBoxSizeProps) {
   const roundedType = isRounded ? "rounded" : "";
   // 背景色の指定も含む
@@ -21,14 +27,24 @@ export default function CommentBox({
     ? "bg-gray-100 cursor-not-allowed"
     : "bg-cp-white";
   return (
-    <textarea
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      disabled={isDisabled}
-      className={`
+    <div className="w-105">
+      {label && <label className="body-cp-medium">{label}</label>}
+      <textarea
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={isDisabled}
+        {...(isMaxLength && maxLength ? { maxLength } : {})}
+        className={`
           ${disabledStyle} ${roundedType}
-          w-105 h-76 body-cp-small pl-2.5 pt-2 focus:outline-none resize-none `}
-    />
+          w-full h-76 body-cp-small pl-2.5 pt-2 mt-1 focus:outline-none resize-none
+        `}
+      />
+      {isMaxLength && maxLength && (
+        <p className=" text-right label-cp-medium text-cp-black">
+          {value.length}/{maxLength} 文字
+        </p>
+      )}
+    </div>
   );
 }

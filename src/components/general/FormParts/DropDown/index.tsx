5650aaf26e7da462bd52cd351de14a1082948c15
西川雅人 <masato.nishikawa@arteryex.biz>
2025-07-22 10:35:54 +0900

    fix:ドロップダウンのインポート文修正

--- src/components/general/FormParts/DropDown/index.tsx
+++ src/components/general/FormParts/DropDown/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 
 type DropBoxSize = "small" | "medium" | "large";
